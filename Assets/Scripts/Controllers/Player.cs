ae34d55b76dd62c0425c28766280009e8d12c2f0
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-09-21 07:40:44 -0400

    Task 2 finished
    When y is pressed a bomb will spawn randomly in one of the four corners of the ship

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,17 +9,69 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
 
+    public Vector2 bombOffset = new Vector2(0, 1);      //Single bomb spawning by pressing b
+
+    public float bombTrailSpacing = 1f;     //Distance between the trail bombs
+    public int numberOfTrailBombs = 3;      //How many bombs to spawn in the trail
+
+    public float randomCornerDistance = 2f;     //Distance from player to each corner
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))        
+        if (Input.GetKeyDown(KeyCode.B))                //Spawns a singular bomb at the fixed location
         {
-            SpawnBombAtOffset(new Vector3(0, 1));  //Same as Vector3.up since it's 0,1, got it from Jeff in class
+            SpawnBombAtOffset(bombOffset);  //Same as Vector3.up since it's 0,1, got it from Jeff in class
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            SpawnBombAtRandomCorner(randomCornerDistance);
         }
     }
 
-    private void SpawnBombAtOffset(Vector3 inOffset)
+
+    private void SpawnBombAtOffset(Vector2 inOffset)
+    {
+        Vector3 spawnPos = transform.position + (Vector3)inOffset;
+        Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+    }
+
+    private void SpawnBombTrail(float bombspacing, int numberOfBombs)
+    {
+        for (int i = 1; i <= numberOfBombs ; i++)
         {
-            Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);        //Got it in class
+            Vector3 offset = Vector3.down * (i * bombspacing);      //Behind is relative to the ship's position, so in this case behind would be Vector3.down
+            Vector3 spawnPos = transform.position + offset;
+
+            Instantiate(bombPrefab,spawnPos, Quaternion.identity); 
         }
+    }
+
+    private void SpawnBombAtRandomCorner(float inDistance)
+    {
+        Vector2 center = transform.position;    //I'm using Vector2 center as the player's location (spaceship)f
+        Vector2 size = new Vector2(inDistance * 2f, inDistance * 2f);
+
+        float halfWidth = size.x * 0.5f;
+        float halfHeight = size.y * 0.5f;
+
+        Vector2 topLeft = new Vector2(center.x - halfWidth, center.y + halfHeight);
+        Vector2 topRight = new Vector2(center.x + halfWidth, center.y + halfHeight);
+        Vector2 bottomRight = new Vector2(center.x + halfWidth, center.y - halfHeight);
+        Vector2 bottomLeft = new Vector2(center.x - halfWidth, center.y - halfHeight);
+
+        Vector2[] corners = { topLeft, topRight, bottomRight, bottomLeft };         //I created an array for the four corners relative to the ship
+
+        int index = Random.Range(0, corners.Length);
+        Vector2 chosen = corners[index];
+
+        Instantiate(bombPrefab, chosen, Quaternion.identity);
+
+    }
 }
