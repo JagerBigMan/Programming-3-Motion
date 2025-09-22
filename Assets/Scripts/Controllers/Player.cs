eab0012b327a9f8e24d39d13cf8070c9420d1ac1
Johnson Doong <johnsondoong20040608@gmail.com>
2025-09-22 09:27:05 -0400

    Imported unity package

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,24 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    public List<Transform> asteroidTransforms;
     public Transform enemyTransform;
     public GameObject bombPrefab;
-    public List<Transform> asteroidTransforms;
+    public Transform bombsTransform;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))        
-        {
-            SpawnBombAtOffset(new Vector3(0, 1));  //Same as Vector3.up since it's 0,1, got it from Jeff in class
-        }
+
     }
 
-    private void SpawnBombAtOffset(Vector3 inOffset)
-        {
-            Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);        //Got it in class
-        }
 }
