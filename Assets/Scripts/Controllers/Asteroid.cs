d2c586c15012ef7612f437f0ace02fc7b06f0c4b
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-09-28 19:48:53 -0400

    Task 3 finished

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public float arrivalDistance = 0.5f;
-    public float maxFloatDistance = 5f;
+    public float moveSpeed = 2f;        //How fast asteroids moves
+    public float arrivalDistance = 0.5f;        //Distance at which asteroid arrives at target
+    public float maxFloatDistance = 5f;         //Max random distance to pick a new target
 
     private Vector3 targetPoint;
 
-    // Start is called before the first frame update
     void Start()
     {
         ChooseNewTarget();
@@ -19,18 +18,21 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = (targetPoint - transform.position).normalized;
+        Vector3 direction = (targetPoint - transform.position).normalized;      //Move toward the current target
         transform.position += direction * moveSpeed * Time.deltaTime;
 
-        if(Vector3.Distance(transform.position, transform.position) < arrivalDistance)
+        if(Vector3.Distance(transform.position, transform.position) < arrivalDistance)      //If close enough, pick a new point
         {
             ChooseNewTarget();
         }    
     }
     private void ChooseNewTarget()
     {
-        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;       //Pick a random direction in a circle with a radius of 1.
+                                                                            //This is sort of like throwing a dart at a dartboard, the random point can be chosen anywhere inside that circle. 
 
         Vector3 offset = new Vector3(randomDirection.x, randomDirection.y, 0f) * maxFloatDistance;
+
+        targetPoint = transform.position + offset;
     }
 }
