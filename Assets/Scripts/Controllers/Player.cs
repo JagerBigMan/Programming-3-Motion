f0a759a26b0530995fb6da7edcc7a7cccfeb6157
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-09-28 18:56:44 -0400

    Finished task 1

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     [Header ("Movement Properties")]
     public float accelerationTime = 3f;
     public float maxSpeed = 5f;
+    public float deceleration = 4f;
     public float acceleration;
     public Vector3 velocity;
 
@@ -23,29 +24,32 @@ public class Player : MonoBehaviour
 
     private void PlayerMovement()
     {
-        //velocity = Vector3.zero; no longer needed cuz I need the speed to add to it, achieving "accelerating"
         acceleration = maxSpeed / accelerationTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //transform.position += new Vector3(-1, 0, 0);  done in class, tried this way to move the ship
             velocity += acceleration * Time.deltaTime * Vector3.left;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            //transform.position += new Vector3(1, 0, 0);
             velocity += acceleration * Time.deltaTime * Vector3.right;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            //transform.position += new Vector3(0, 1, 0);
             velocity += acceleration * Time.deltaTime * Vector3.up;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            //transform.position += new Vector3(0, -1, 0);
             velocity += acceleration * Time.deltaTime * Vector3.down;
         }
+        if(!Input.GetKey(KeyCode.LeftArrow) &&
+           !Input.GetKey(KeyCode.RightArrow) &&
+           !Input.GetKey(KeyCode.UpArrow) &&
+           !Input.GetKey (KeyCode.DownArrow))
+        {
+            velocity = Vector3.MoveTowards(velocity,Vector3.zero, deceleration * Time.deltaTime);
+        }
+
 
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);      //Limits the max speed by clamping
         transform.position += Time.deltaTime * velocity;
