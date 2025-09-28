062791d1b2a2a870996bfbac94c17f49fffe3ce4
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-09-28 19:23:51 -0400

    Task 2 finished

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
+    public float moveSpeed = 2f;
+    public float arrivalDistance = 0.5f;
+    public float maxFloatDistance = 5f;
+
+    private Vector3 targetPoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        ChooseNewTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = (targetPoint - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        if(Vector3.Distance(transform.position, transform.position) < arrivalDistance)
+        {
+            ChooseNewTarget();
+        }    
+    }
+    private void ChooseNewTarget()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+
+        Vector3 offset = new Vector3(randomDirection.x, randomDirection.y, 0f) * maxFloatDistance;
     }
 }
--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor.Rendering;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;        //Reference to the player
+    public Transform pointA;        //Start of patrol path
+    public Transform pointB;        //End of patrol path 
+    public float moveSpeed = 3f;    
+    public float detectionRange = 6f;
+
+    private Transform currentTarget;    //Where the enemy is moving to
+
+    private void Start()
+    {
+        currentTarget = pointA;
+    }
     private void Update()
     {
-    }
+        if (player == null) return;
 
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if(distanceToPlayer < detectionRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+    private void Patrol()
+    {
+        Vector3 direction = (currentTarget.position - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, currentTarget.position) < 0.2f)
+        {
+            if (currentTarget == pointA)
+            {
+                currentTarget = pointB;
+            }
+            else
+            {
+                currentTarget = pointA;
+            }
+        }
+    }
+    private void ChasePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
 }
